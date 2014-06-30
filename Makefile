11dce64150c904369809fc5d7cd4a4355e88def6
pyotrgalois <federico.carrone@gmail.com>
2014-06-29 21:06:49 -0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+PROJECT = erltool
+
+include erlang.mk
---
+++ src/erltool.erl
@@ -0,0 +1,11 @@
+-module(erltool).
+-export([
+         start/0
+        ,init/0
+        ]).
+
+start() ->
+    init().
+
+init() ->
+    ok.
