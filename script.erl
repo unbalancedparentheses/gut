48f6df135b6574122a60827851b569b5222cd178
Federico Carrone <federico.carrone@gmail.com>
2014-09-11 20:13:59 -0300

    working version

--- script.erl
+++ script.erl
@@ -6,29 +6,43 @@ main(Args) ->
 
 file_tree(Path) ->
     Result = filelib:wildcard("**/*", Path),
-    lists:delete("goro.config", Result).
+    ResultNoDir = lists:filter(fun (X) ->
+                                       not filelib:is_dir(path(Path, X))
+                               end, Result),
+    lists:delete("goro.config", ResultNoDir).
 
 update_all_files(Path) ->
     Files = file_tree(Path),
     io:format("Files ~p~n", [Files]),
     {ok, Patterns} = file:consult(filename:join(Path, "goro.config")),
+    PatternValues = user_values(Patterns),
     lists:foreach(
       fun (File) ->
               Filename = filename:join(Path, File),
-              update(Filename, Patterns)
+              update(Filename, PatternValues)
       end,
       Files).
 
+user_values(Patterns) ->
+    lists:foldl(
+      fun ({Name, Pattern}, Acc) ->
+              {ok, [Value]} = io:fread("Yo mamita, muestrame esa colita ", "~s"),
+              [{Name, Pattern, Value} | Acc]
+      end,
+      [],
+      Patterns).
+
 update(File, Patterns) ->
     lists:foreach(
-      fun ({VariableName, Variable}) ->
-              {ok, [Value]} = io:fread("Yo mamita, muestrame esa colita ", "~s"),
+      fun ({VariableName, Variable, Value}) ->
               BValue = erlang:list_to_binary(Value),
               render(File, Variable, BValue)
       end, Patterns).
-
 
 render(File, Variable, Value) ->
     {ok, Content} = file:read_file(File),
     NewContent = binary:replace(Content, Variable, Value, [global]),
     file:write_file(File, NewContent).
+
+path(Path, File) ->
+    filename:join(Path, File).
