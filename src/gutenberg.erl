e02add53295c8e594fd9bd689dcdd7f844e4bf59
Juan Facorro <Juan Facorro>
2014-09-18 00:53:42 -0300

    Parse options for the command line interface.

--- src/gutenberg.erl
+++ src/gutenberg.erl
@@ -7,9 +7,7 @@
          stop/1
         ]).
 
-main(_) ->
-    check_needed_executables(),
-    io:format("Hola").
+%%% Exported functions
 
 start() ->
     {ok, _Started} = application:ensure_all_started(gutenberg).
@@ -22,6 +20,86 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+main(Args) ->
+    check_needed_executables(),
+    OptSpecList = option_spec_list(),
+    case getopt:parse(OptSpecList, Args) of
+        {ok, {[], []}} ->
+            help();
+        {ok, {Options, Commands}} ->
+            process_options(Options, Commands);
+        {error, {Reason, Data}} ->
+            io:format("Error: ~s ~p~n~n", [Reason, Data]),
+            help()
+    end.
+
+%%% Internal
+
+%% Command line options
+
+-spec process_options([atom()], [string()]) -> ok.
+process_options([help | Opts], Cmds) ->
+    help(),
+    process_options(Opts, Cmds);
+process_options([commands | Opts], Cmds) ->
+    commands(),
+    process_options(Opts, Cmds);
+process_options([], Cmds) ->
+    process_commands(Cmds).
+
+-spec process_commands([string()]) -> ok.
+process_commands(["new" | Cmds]) ->
+    new(Cmds);
+process_commands(["list" | Cmds]) ->
+    list(),
+    process_commands(Cmds);
+process_commands([]) ->
+    ok;
+process_commands([_Cmd | _Cmds]) ->
+    throw(unrecognized_or_unimplemened_command).
+
+%% Commands
+
+new(Args) ->
+    io:format("~p~n", [Args]).
+
+list() ->
+    io:format("Retrieving all generators...~n"),
+    Generators = gute_generators:find_all(),
+    Fun = fun(#{name := Name, description := Desc}) ->
+                  io:format("~s\t~s~n", [Name, Desc])
+          end,
+    lists:foreach(Fun, Generators).
+
+%% Options
+
+help() ->
+    OptSpecList = option_spec_list(),
+    getopt:usage(OptSpecList, atom_to_list(?MODULE), standard_io).
+
+-spec commands() -> ok.
+commands() ->
+    Commands = <<"Gutenberg can perform this awesome commands for you:
+
+new GEN-TPLT NAME [VARIABLE=VALUE]
+               Create a new project using a generator, or a new file
+               using a template. GEN-TPLT is the generator's or
+               template's name. NAME should be the value for the
+               variable of the same name. Subsequent VALUE specifications
+               should be paired with its VARIABLE name.
+
+list           List all available generators and templates.
+">>,
+   io:put_chars(Commands).
+
+option_spec_list() ->
+    [
+     {help, $h, "help", undefined, "Show this help information."},
+     {commands, undefined, "commands", undefined, "Show available commands."}
+    ].
+
+%% Executables checks
 
 needed_executables() ->
     [
