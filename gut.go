68d80cba91377398d46fc5ada3daa041728433d7
unbalancedparentheses <federico.carrone@gmail.com>
2016-03-09 23:38:37 -0300

    Change concatenation of paths by join function

--- gut.go
+++ gut.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -14,7 +15,6 @@ import (
 	"github.com/google/go-github/github"
 	"github.com/hoisie/mustache"
 	git "github.com/libgit2/git2go"
-
 	"gopkg.in/yaml.v2"
 )
 
@@ -120,7 +120,8 @@ func search(filter string) {
 func new(templateName string) {
 	repoName := templateName + suffix
 	repoURL := "https://github.com/" + repoName + ".git"
-	fullPath = cwd + "/" + name + "/"
+
+	fullPath = path.Join(cwd, name)
 	gitopts := &git.CloneOptions{}
 
 	boldWhite.Printf("Retrieving %s via %s\n", templateName, repoURL)
@@ -132,7 +133,7 @@ func new(templateName string) {
 		println()
 	}
 
-	config(name, fullPath+templateFile)
+	config(name, path.Join(fullPath, templateFile))
 	askForVariables()
 	clean()
 	compile()
@@ -170,9 +171,9 @@ func askForVariables() {
 }
 
 func clean() {
-	os.Remove(fullPath + templateFile)
-	os.RemoveAll(fullPath + ".git/")
-	os.Remove(fullPath + ".gitignore")
+	os.Remove(path.Join(fullPath, templateFile))
+	os.RemoveAll(path.Join(fullPath, ".git/"))
+	os.Remove(path.Join(fullPath, ".gitignore"))
 }
 
 func compile() {
@@ -194,7 +195,11 @@ func compileDirs(fullPath string, f os.FileInfo, err error) error {
 }
 
 func printCreating(path string) {
-	relativePath := strings.TrimPrefix(path, cwd+"/")
+	relativePath, err := filepath.Rel(cwd, path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	color.New(color.FgGreen).Print("* creating")
 	fmt.Printf(" %s\n", relativePath)
@@ -262,15 +267,11 @@ func printCommand(command string, arguments []string) {
 }
 
 // inmediate tasks:
-// use paths functions instead of concatenaning strings
-// add curl command in makefile like docker compose has
-// paginate to get all answers
+// add curl command in readme like docker compose has. generate gox builds
+// work on temporary directory? if yes, then use os tempdir function. think if this is useful
 // commands not be inside of an array in gut.template
 // check that gut.template is present
-// work on temporary directory? if yes, then use os tempdir function. don't know if this is useful
-
-// new features:
-// name is the string after the last slash from the provided name. always work on current working dir. check if this is the default behaviour on other similar tools
+// name in the string should not have a / slash. if it has, we should only use the first
 // add describe template subcommand
 // clone templates with depth one
 // ask for specific tag or commit id of generator
@@ -279,3 +280,7 @@ func printCommand(command string, arguments []string) {
 // store templates in /home/.gut/templates/. only fetch if template not found in that path
 // update stored templates
 // add option to fill variables from a file
+// add homebrew formulas
+// paginate to get all answers
+// add documentation in the readme
+// add version in gut help
